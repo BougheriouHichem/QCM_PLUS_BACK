14a690878cde42b4fecaf6dc3cd98e027bf00d53
bougheriouHichem <bougheriou1997@gmail.com>
2024-07-25 18:39:24 +0200

    Amélioration Gestion QuestionnaireService

--- src/main/java/com/pfe/qcm_plus_back/service/QuestionnaireService.java
+++ src/main/java/com/pfe/qcm_plus_back/service/QuestionnaireService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionnaireService {
@@ -18,13 +19,20 @@ public class QuestionnaireService {
     }
 
     public Questionnaire updateQuestionnaire(Long id, Questionnaire questionnaireDetails) {
-        Questionnaire questionnaire = questionnaireRepository.findById(id)
+        Questionnaire existingQuestionnaire = questionnaireRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Questionnaire not found on :: " + id));
 
-        questionnaire.setName(questionnaireDetails.getName());
-        questionnaire.setDescription(questionnaireDetails.getDescription());
-        return questionnaireRepository.save(questionnaire);
+        // Mise à jour du nom si présent
+        Optional.ofNullable(questionnaireDetails.getName())
+                .ifPresent(existingQuestionnaire::setName);  // Met à jour le nom seulement s'il est fourni
+
+        // Mise à jour de la description si présente
+        Optional.ofNullable(questionnaireDetails.getDescription())
+                .ifPresent(existingQuestionnaire::setDescription);  // Met à jour la description seulement si elle est fournie
+
+        return questionnaireRepository.save(existingQuestionnaire);
     }
+
 
     public void deleteQuestionnaire(Long id) {
         Questionnaire questionnaire = questionnaireRepository.findById(id)
